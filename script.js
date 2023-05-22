99a9d4851ada088746dd9ebf4e65418897394dad
SVigramR <87016515+SVigramR@users.noreply.github.com>
2023-05-22 08:23:26 +0530

    Added players and eventlisteners for the gameboard
    • Function that switches players after each click.
    • Event listener that runs through each block of the gameboard, then updates the 2D array and UI. Also it removes the event listener after clicked.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const player1 = "O";
+const player2 = "X";
+
+let currentPlayer = player1;
+
+function switchPlayer() {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+}
+
 function createGameArray(row, column, value) {
     let array = [];
     for (let i = 0; i < row; i++) {
@@ -17,8 +26,6 @@ function createGameArray(row, column, value) {
 }
 
 let gameArray = createGameArray(3, 3, "");
-// Set a cell in the 2D array
-gameArray.setCell(0, 1, "O");
 
 // Get a copy of the 2D array
 let copy = gameArray.getGameArray();
@@ -31,3 +38,72 @@ const gameboard = () => {
 const validate = () => {
     // TTT validation code here
 };
+
+const gameBox = document.querySelectorAll(".gamebox");
+
+gameBox.forEach(function (box, index) {
+    box.addEventListener("click", function boxClick(event) {
+        const clickedCell = event.target;
+        clickedCell.textContent = currentPlayer;
+        let updateArray;
+        switch (index) {
+            case 0:
+                gameArray.setCell(0, 0, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                console.log(updateArray);
+                event.target.removeEventListener("click", boxClick);
+                break;
+            case 1:
+                gameArray.setCell(0, 1, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                console.log(updateArray);
+                event.target.removeEventListener("click", boxClick);
+                break;
+            case 2:
+                gameArray.setCell(0, 2, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                console.log(updateArray);
+                event.target.removeEventListener("click", boxClick);
+                break;
+            case 3:
+                gameArray.setCell(1, 0, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                event.target.removeEventListener("click", boxClick);
+                console.log(updateArray);
+                break;
+            case 4:
+                gameArray.setCell(1, 1, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                event.target.removeEventListener("click", boxClick);
+                console.log(updateArray);
+                break;
+            case 5:
+                gameArray.setCell(1, 2, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                event.target.removeEventListener("click", boxClick);
+                console.log(updateArray);
+                break;
+            case 6:
+                gameArray.setCell(2, 0, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                event.target.removeEventListener("click", boxClick);
+                console.log(updateArray);
+                break;
+            case 7:
+                gameArray.setCell(2, 1, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                event.target.removeEventListener("click", boxClick);
+                console.log(updateArray);
+                break;
+            case 8:
+                gameArray.setCell(2, 2, currentPlayer);
+                updateArray = gameArray.getGameArray();
+                event.target.removeEventListener("click", boxClick);
+                console.log(updateArray);
+                break;
+            default:
+                break;
+        }
+        switchPlayer();
+    });
+});

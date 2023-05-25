b63a4bd977122e263702e571e7bd1d7f7413cd6c
SVigramR <87016515+SVigramR@users.noreply.github.com>
2023-05-26 04:34:33 +0530

    Completed Tier-1 playable TTT
    • Removed foreach which adds event listener to each box and made it a function which can be later recalled in separate function that contain event listeners.
    • Added refresh function which clears the game data.
    • Added restart button with restart function which uses refresh function, and creates a new game.
    • Added a function to remove events when there is a result for the game.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const player1 = "O";
 const player2 = "X";
-
+const result = document.querySelector(".result");
+let gameBox = document.querySelectorAll(".gamebox");
 let currentPlayer = player1;
 
 function switchPlayer() {
@@ -75,61 +76,94 @@ function createGameArray(row, column, value) {
 
 let gameArray = createGameArray(3, 3, "");
 
-// Get a copy of the 2D array
-let copy = gameArray.getGameArray();
-console.log(copy);
+function justClick(event) {
+    let para = document.createElement("p");
+    para.classList.add("box");
+    event.currentTarget.appendChild(para);
+    para.textContent = currentPlayer;
+    const index = Array.from(gameBox).indexOf(event.currentTarget);
+    switch (index) {
+        case 0:
+            gameArray.setCell(0, 0, currentPlayer);
+            break;
+        case 1:
+            gameArray.setCell(0, 1, currentPlayer);
+            break;
+        case 2:
+            gameArray.setCell(0, 2, currentPlayer);
+            break;
+        case 3:
+            gameArray.setCell(1, 0, currentPlayer);
+            break;
+        case 4:
+            gameArray.setCell(1, 1, currentPlayer);
+            break;
+        case 5:
+            gameArray.setCell(1, 2, currentPlayer);
+            break;
+        case 6:
+            gameArray.setCell(2, 0, currentPlayer);
+            break;
+        case 7:
+            gameArray.setCell(2, 1, currentPlayer);
+            break;
+        case 8:
+            gameArray.setCell(2, 2, currentPlayer);
+            break;
+        default:
+            break;
+    }
+    event.currentTarget.removeEventListener("click", justClick);
+    let updateArray = gameArray.getGameArray();
+    console.log(updateArray);
+    if (gameArray.checkForWin(updateArray) === "O") {
+        result.textContent = "Player1 Won the Game!";
+        removeEventListeners();
+    }
+    if (gameArray.checkForWin(updateArray) === "X") {
+        result.textContent = "Player2 Won the Game!";
+        removeEventListeners();
+    }
+    if (gameArray.checkForTie(updateArray) === true) {
+        result.textContent = "It's a Tie!";
+        removeEventListeners();
+    }
+    switchPlayer();
+}
 
-const gameBox = document.querySelectorAll(".gamebox");
-const result = document.querySelector(".result");
-
-gameBox.forEach(function (box, index) {
-    box.addEventListener("click", function boxClick(event) {
-        let para = document.createElement("p");
-        box.appendChild(para);
-        para.textContent = currentPlayer;
-        switch (index) {
-            case 0:
-                gameArray.setCell(0, 0, currentPlayer);
-                break;
-            case 1:
-                gameArray.setCell(0, 1, currentPlayer);
-                break;
-            case 2:
-                gameArray.setCell(0, 2, currentPlayer);
-                break;
-            case 3:
-                gameArray.setCell(1, 0, currentPlayer);
-                break;
-            case 4:
-                gameArray.setCell(1, 1, currentPlayer);
-                break;
-            case 5:
-                gameArray.setCell(1, 2, currentPlayer);
-                break;
-            case 6:
-                gameArray.setCell(2, 0, currentPlayer);
-                break;
-            case 7:
-                gameArray.setCell(2, 1, currentPlayer);
-                break;
-            case 8:
-                gameArray.setCell(2, 2, currentPlayer);
-                break;
-            default:
-                break;
-        }
-        event.target.removeEventListener("click", boxClick);
-        let updateArray = gameArray.getGameArray();
-        console.log(updateArray);
-        if (gameArray.checkForWin(updateArray) === "O") {
-            result.textContent = "Player1 Won the Game!"
-        }
-        if (gameArray.checkForWin(updateArray) === "X") {
-            result.textContent = "Player2 Won the Game!"
-        }
-        if (gameArray.checkForTie(updateArray) === true ) {
-            result.textContent = "It's a Tie!"
-        }
-        switchPlayer();
+function refresh() {
+    currentPlayer = player1;
+    let refreshGameBox = document.querySelectorAll(".gamebox");
+    refreshGameBox.forEach(function (box) {
+        let removePara = box.querySelectorAll(".box");
+        removePara.forEach(function (subBox) {
+            box.removeChild(subBox);
+        });
     });
-});
+    result.textContent = "Player O's Turn";
+}
+
+function restart() {
+    refresh();
+    gameArray = createGameArray(3, 3, "");
+    boxClick();
+}
+
+function boxClick() {
+    gameBox.forEach(function (box) {
+        box.addEventListener("click", justClick);
+    });
+}
+
+function removeEventListeners() {
+    gameBox.forEach(function (box) {
+        box.removeEventListener("click", justClick);
+    });
+}
+
+let restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", function () {
+    restart();
+}); 
+
+boxClick();
